3b6e3c03b6b8098af9f564d983b1797d33cd9c2d
Travis Nguyen <travishn@uci.edu>
2018-04-30 18:12:47 -0700

    Finish 3/4 of questions

---
+++ skeleton/lib/03a_nobel_bonus.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+# BONUS PROBLEM: requires sub-queries or joins. Attempt this after completing
+# sections 04 and 07.
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+    SELECT
+      DISTINCT yr
+    FROM
+      nobels
+    WHERE
+      subject = 'Physics'
+    GROUP BY
+      yr
+    HAVING
+      subject != 'Chemistry'
+  SQL
+end
